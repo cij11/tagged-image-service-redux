7a720f744c54b32a2c57fad3493ebaa1311419e1
Chris Jolly <chris.jolly@sykescottages.co.uk>
2022-09-04 12:17:19 +1200

    Unit tests for Local auth strategy

---
+++ test/src/auth/local.strategy.spec.ts
@@ -0,0 +1,48 @@
+import * as sinon from 'sinon'
+import { AuthService } from '../../../src/auth/auth.service'
+import { LocalStrategy } from '../../../src/auth/local.strategy'
+
+describe('LocalStrategy.validate', () => {
+    it('should return an AuthenticatedUser, if authService returns a valid user', async () => {
+        // Given
+        const username = 'chris.jolly'
+        const password = 'Password123!'
+
+        const authenticatedUser: AuthenticatedUser = {
+            userId: 1,
+            username
+        }
+
+        const authService = sinon.createStubInstance(AuthService)
+        authService.validateUser.resolves(authenticatedUser)
+
+        const sut = new LocalStrategy(authService)
+
+        // When
+        const actualAuthenticatedUser = await sut.validate(username, password)
+
+        // Then
+        expect(actualAuthenticatedUser).toStrictEqual(authenticatedUser)
+    })
+
+    it('should throw an Unauthorized exception, if authService does not return a valid user', async () => {
+        // Given
+        const username = 'chris.jolly'
+        const password = 'Password123!'
+
+        const authenticatedUser: AuthenticatedUser = {
+            userId: 1,
+            username
+        }
+
+        const authService = sinon.createStubInstance(AuthService)
+        authService.validateUser.resolves(null)
+
+        const sut = new LocalStrategy(authService)
+
+        // When + Then
+        await expect(sut.validate(username, password)).rejects.toThrow(
+            'Unauthorized'
+        )
+    })
+})
