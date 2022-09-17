13b59feec966e1e5fac2ea2cf30b58ae0d3a78ef
Chris Jolly <chris.jolly@sykescottages.co.uk>
2022-09-17 13:18:19 +1200

    Check JWT Authorization and Audit requests to /tags endpoints

--- src/tag/tag.controller.ts
+++ src/tag/tag.controller.ts
@@ -1,5 +1,14 @@
-import { Body, Controller, Get, Post } from '@nestjs/common'
+import {
+    Body,
+    Controller,
+    Get,
+    Post,
+    UseGuards,
+    UseInterceptors
+} from '@nestjs/common'
+import { JwtAuthGuard } from '@src/auth/jwt-auth.guard'
 import { Tag } from '@src/entity/tag.entity'
+import { AuditInterceptor } from '@src/logging/audit.interceptor'
 import { TagService } from '@src/tag/tag.service'
 
 @Controller('tags')
@@ -7,11 +16,14 @@ export class TagController {
     constructor(private readonly tagService: TagService) {}
 
     @Get()
+    @UseGuards(JwtAuthGuard)
     findAll(): Promise<Tag[]> {
         return this.tagService.findAll()
     }
 
     @Post()
+    @UseGuards(JwtAuthGuard)
+    @UseInterceptors(AuditInterceptor)
     async create(@Body() tag: Tag) {
         return this.tagService.create(tag)
     }
--- src/tag/tag.module.ts
+++ src/tag/tag.module.ts
@@ -1,11 +1,18 @@
 import { Module } from '@nestjs/common'
+import { AuditRepository } from '@src/audit/audit.repository'
+import { AuditService } from '@src/audit/audit.service'
 import { TagController } from '@src/tag/tag.controller'
 import { TagRepository } from '@src/tag/tag.repository'
 import { TagService } from '@src/tag/tag.service'
 
 @Module({
     imports: [],
-    providers: [TagService, TagRepository],
+    providers: [
+        TagService,
+        TagRepository,
+        AuditService, // Include AuditService at app module level to be available to AuditInterceptor
+        AuditRepository // Include AuditRepository at app module level to be available to AuditInterceptor
+    ],
     controllers: [TagController]
 })
 export class TagModule {}
